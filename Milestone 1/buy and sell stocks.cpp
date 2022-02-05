586370deaf621cf40d01209777f921bf2b2820f7
comrade-buck <92451998+comrade-buck@users.noreply.github.com>
2022-02-05 20:44:16 +0530

    Update buy and sell stocks.cpp

--- Milestone 1/buy and sell stocks.cpp
+++ Milestone 1/buy and sell stocks.cpp
@@ -1,1 +1,13 @@
-gg
+int n=prices.size();
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1])
+                ans += prices[i] - prices[i - 1];
+        }
+        return ans;
+    }
+int main() 
+{ 
+ vector<int> arr = { 7,1,5,3,6,4 }; 
+ cout<<maxProfit(arr)<<endl; 
+ return 0;
